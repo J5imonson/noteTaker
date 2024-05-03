7471879da96428e8697bf54b1413b2bfca268daf
CodeChef <joetomsim@gmail.com>
2024-05-02 21:30:07 -0500

    added the post route for api/notes

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-/*
-TODO:
-
-API routes:
-
--POST/api/notes should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
--add old note to new note, push new note into db.json, then re-write file
--download an npm pack for the id
-*/
-
 const express = require("express");
 const app = express();
-const PORT = 3001
+const PORT = process.envPORT || 3001;
 const path = require("path");
 const noteData = require("./db/db.json");
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+app.use(express.json());
 
 app.use(express.static("public"));
 
@@ -20,11 +14,25 @@ app.get("/", (req, res) => res.send("directs to index.html"));
 
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
+app.get("/api/notes", (req, res) => res.send(noteData));
+
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
-app.get("/api/notes", (req, res) => res.json(noteData));
+app.post("/api/notes", (req, res) => {
+  console.log(req.body);
+  const { title, text } = req.body;
+  const id = uuidv4();
+  const newNote = { id, title, text };
 
-app.post("/api/notes", (req, res) => );
+  // Push the combined note into the array of notes
+  console.log(newNote);
+  noteData.push(newNote);
+  console.log(noteData);
+  
+  // Write the updated notes back to db.json
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteData, null, 4 ));
+  res.json(noteData);
+});
 
 app.listen(PORT, () =>
   console.log(`server listening at http://localhost:${PORT}`)
